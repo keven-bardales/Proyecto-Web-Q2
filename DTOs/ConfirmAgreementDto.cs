c02f1624ecc7bbf2d60052c9932f2e4d23cfa295
numbpi <rimv715@gmail.com>
2026-06-03 20:31:34 -0600

    feat: Agregando los Dto para los acuerdos
    - ConfirmeAgreementDto -> Cuando vayan a confirmar el acuerdo, es mas
    para cerrar el caso
    - CreateAgreementDto -> Es para la estructura de la creacion de los
    acuerdos

--- DTOs/ConfirmAgreementDto.cs
+++ DTOs/ConfirmAgreementDto.cs
@@ -1,3 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Proyecto_Web_Q2.DTOs;
 
-public class ConfirmAgreementDto { }
+public class ConfirmAgreementDto
+{
+    [Required]
+    public string AgreementId = string.Empty;
+
+    [Required]
+    public bool Confirmed = false;
+}
--- DTOs/CreateAgreementDto.cs
+++ DTOs/CreateAgreementDto.cs
@@ -1,3 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Proyecto_Web_Q2.DTOs;
 
-public class CreateAgreementDto { }
+public class CreateAgreementDto
+{
+    [Required]
+    public string CaseId = string.Empty;
+
+    [Required]
+    public string AgreementText = string.Empty;
+
+    [Required]
+    [MinLength(1)]
+    public List<CreateAgreementPointDto> Points = [];
+}
+
+// TODO: Le debo consultar al Ingeniero si muevo esto aun Dto aparte o lo dejo aca insanamente
+public class CreateAgreementPointDto
+{
+    [Required]
+    public string Description = string.Empty;
+
+    [Required]
+    public DateTime Deadline = DateTime.UtcNow;
+}
