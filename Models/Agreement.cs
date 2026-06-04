042a0b544b6979a32e6fa6b8d1a3d033642f73a9
numbpi <rimv715@gmail.com>
2026-06-03 20:07:52 -0600

    feat: Agregando los Modelo de los siguiente acuerdos: - AgreementPoint -> Pues este seria mas para el mediador para el cierre del caso - Agreement -> Como va ir estructurado el acuerdo

--- Models/Agreement.cs
+++ Models/Agreement.cs
@@ -1,3 +1,13 @@
 namespace Proyecto_Web_Q2.Models;
 
-public class Agreement { }
+public class Agreement
+{
+    public string Id { get; set; } = string.Empty; // El Id  del Acuerdo
+    public string CaseId { get; set; } = string.Empty; // El Id del Caso
+    public string MediatorId { get; set; } = string.Empty; // El Id del mediador
+    public List<AgreementPoint> Points { get; set; } = []; // Seria los puntos que se ven
+    public bool ConfirmedByReporter = false; // Cuando llega a confirmar el que reporto
+    public bool ConfirmedByRespondent = false; // Cuando llega a confirmar el acusado o el jodido xd
+    public DateTime? FormalizedAt = null; // Cuando se llego a una conclusión del acuerdo
+    public DateTime CreatedAt = DateTime.UtcNow; // Cuando se creo el acuerdo
+}
--- Models/AgreementPoint.cs
+++ Models/AgreementPoint.cs
@@ -1,3 +1,21 @@
 namespace Proyecto_Web_Q2.Models;
 
-public class AgreementPoint { }
+public class AgreementPoint
+{
+    /*
+     * Este seria como el reporte de lo que va escribir el mediador
+     * del acuerdo que se llego con las personas sobre el caso
+     */
+    public string Description { get; set; } = string.Empty;
+
+    // Este solo seria para el cierre del acuerdo
+    public DateTime Deadline { get; set; } = DateTime.UtcNow;
+
+    /*
+      Los que vamos a usar en ComplianceStatus seran:
+      - pendiente -> Es el default
+      - cumplido
+      - incumplido
+     */
+    public string ComplianceStatus { get; set; } = "pendiente";
+}
