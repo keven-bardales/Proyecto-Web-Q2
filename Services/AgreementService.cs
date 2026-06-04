c2bb7a8ff9794a94d2bff3f6a9d7f45ada875132
numbpi <rimv715@gmail.com>
2026-06-03 20:57:23 -0600

    todo: Terminar el primer metodo de los agreements que es solo para crear el acuerdo
    La idea es que confirme si el caso existe, si el usuario es mediador y
    puede ser que revise el estado del acuerdo para ver si es nuevo que se
    pueda hacer el acerdo pero si no es,no lo deje. Se supone xd

--- Services/AgreementService.cs
+++ Services/AgreementService.cs
@@ -1,3 +1,35 @@
+using Proyecto_Web_Q2.DTOs;
+
 namespace Proyecto_Web_Q2.Services;
 
-public class AgreementService { }
+public class AgreementService
+{
+    private readonly FireBaseService _fireBaseService;
+    private readonly string collectionName = "agreements";
+
+    public AgreementService(FireBaseService fireBase)
+    {
+        _fireBaseService = fireBase;
+    }
+
+    public async Task<CreateAgreementDto> CreateAsync(CreateAgreementDto dto, string userId)
+    {
+        var caseDoc = await _fireBaseService
+            .GetCollection("cases")
+            .Document(dto.CaseId)
+            .GetSnapshotAsync();
+
+        if (!caseDoc.Exists)
+            throw new Exception("No se encuentro ningun caso");
+
+        string userMediatorId = caseDoc.GetValue<string>("MediadorId");
+
+        var mediatorsDoc = await _fireBaseService
+            .GetCollection("mediators")
+            .Document(dto.CaseId)
+            .GetSnapshotAsync();
+
+        if (!mediatorsDoc.Exists)
+            throw new Exception("El usuario no es un mediador asignado");
+    }
+}
